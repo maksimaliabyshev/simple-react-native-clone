5036b4a7e53d583eee159b6ae49ac803721a39f9
Maksim Alaybyshev <truepatch@gmail.com>
2021-02-08 03:35:07 +0500

    change config

--- clone.config.js
+++ clone.config.js
@@ -9,7 +9,6 @@ const cloneConfig = {
     },
     build_ios: ['npx react-native run-ios --configuration "Release"'],
     build_android: [
-        // 'cd ${clone.destFull}',
         'cd ./android',
         'pwd',
         // './gradlew cleanBuildCache',
@@ -38,6 +37,16 @@ const cloneConfig = {
         'web-build',
         'app/config/env.*.js',
     ],
+    copy_global: [
+        {
+            from: './ios/rn_starter/Images_${clone.nameProject}.xcassets',
+            to: './ios/rn_starter/Images.xcassets',
+        },
+        {
+            from: './android/app/src/main/res_${clone.nameProject}',
+            to: './android/app/src/main/res',
+        },
+    ],
     // replace_global: [
     //     {
     //         files: './android/app/build.gradle',
@@ -46,35 +55,29 @@ const cloneConfig = {
     //         to: ['$1${clone.package}$3'],
     //     },
     // ],
-    copy_global: [
-        {
-            from: './ios/rn_starter/Images_${clone.nameProject}.xcassets',
-            to: './ios/rn_starter/Images.xcassets',
-        },
-    ],
     clone: [
         {
             folder: '../rn_starter_company',
-            name: 'Name App Company',
-            package: 'com.rn_starter.company.name',
-            copy: [
-                {
-                    from: './android/app/src/main/res_${clone.nameProject}',
-                    to: './android/app/src/main/res',
-                },
-            ],
+            name: 'New AppName',
+            package: 'com.rn_starter.company',
+            // copy: [
+            //     {
+            //         from: './android/app/src/main/res_${clone.nameProject}',
+            //         to: './android/app/src/main/res',
+            //     },
+            // ],
         },
         {
             folder: '../rn_starter_company2',
             package: 'com.rn_starter.company2',
-            name: 'App Name2',
-            copy: [
-                {
-                    from:
-                        './android/app/src/main/res/playstore_${clone.nameProject}.png',
-                    to: './android/app/src/main/res/playstore.png',
-                },
-            ],
+            name: 'New AppName 2',
+            // copy: [
+            //     {
+            //         from:
+            //             './android/app/src/main/res/playstore_${clone.nameProject}.png',
+            //         to: './android/app/src/main/res/playstore.png',
+            //     },
+            // ],
             // replace: {
             //     files: './android/app/build.gradle',
             //     from: '/(applicationId\\s*")(.+)(")/giu',
